6c2c6c03ac4594b94d4a564ef5ae5b345cf5e076
jrseph <ppyjo3@nottingham.ac.uk>
2023-01-07 15:52:12 +0000

    Update PEC code v1.py

--- PEC code v1.py
+++ PEC code v1.py
@@ -2,39 +2,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate
 
-    
-def PSF_lr(r, beta, eta):
+
+def PSF_lr():
     '''
-    Point-spread function as a long-range Gaussian only
+    Normalised point-spread function as a long-range Gaussian only
+    Each array value corresponds to 100nm
     
-    r : radial position (um)
     beta : long-range scattering characteristic length (um)
     eta : Gaussian weighting (dimensionless)
     '''
-    # PSF = 1/(np.pi * (1 + eta)) * eta/(beta**2) * np.exp(-np.abs((r)**2)/beta**2)
-    PSF = eta/(beta**2) * np.exp(-np.abs((r)**2)/beta**2)
-    PSF_norm = PSF / np.amax(PSF)
-    return PSF, PSF_norm
+    # PSF runs from -3*beta to 3*beta; each r val corresponds to 100nm if beta is given in um
+    r = np.arange(-3*round(beta), 3*round(beta), 0.1)
+    PSF = 1/(np.pi*(1+eta)) * eta/(beta**2) * np.exp(-np.abs(r**2)/beta**2)
+    PSF_norm = PSF / integrate.simps(PSF)
+    return PSF_norm
 
 
-def PSF_pl(r, beta, gamma, eta, sigma, nu):
+def PSF_pl():
     '''
-    Point-spread function as a (short-range) power law plus a long-range Gaussian
+    Normalised oint-spread function as a (short-range) power law plus a long-range Gaussian
+    Each array value corresponds to 100nm
     
-    r : radial position (um)
     beta : long-range scattering characteristic length (um)
     eta : Gaussian weighting (dimensionless)
     sigma : power-law scaling of intermdiate range (dimensionless)
     gamma : short range over which dose distribution is mostly constant (nm)
     nu : chosen to normalise PSF (nm)
     '''
-    PSF = 1/(np.pi * (1 + eta)) * (nu**(-2)/(1 + (np.abs(r)/gamma)**sigma) + eta/(beta**2) * np.exp(-np.abs((r)**2)/beta**2))
-    PSF_norm = PSF / np.amax(PSF)
-    return PSF, PSF_norm
+    # PSF runs from -3*beta to 3*beta; each r val corresponds to 100nm if beta is given in um
+    r = np.arange(-3*round(beta), 3*round(beta), 0.1)
+    PSF = 1/(np.pi*(1+eta)) * (nu**(-2)/(1 + (np.abs(r)/gamma)**sigma) \
+                               + eta/(beta**2) * np.exp(-np.abs((r)**2)/beta**2))
+    PSF_norm = PSF / integrate.simps(PSF)
+    return PSF_norm
 
 
-# %%
-# From table 1 on poster
+# Scattering parameters
 beta = 28.5     # um, long range scattering
 eta = 0.75      # dimensionless
 gamma = 8.01    # nm
@@ -43,18 +46,20 @@ nu = 18.3       # nm
 
 # Porperties of substrate
 subs_width = 200 # substrate width (um)
-n_cells = subs_width * 1000   # number of r values such that each corresponds to a cell of width 1nm
-r = np.linspace(-subs_width/2, subs_width/2, n_cells)
+n_cells = subs_width * 1000   # number of r values such that each corresponds to a cell of width 100nm
+x = np.linspace(-subs_width/2, subs_width/2, n_cells)
 
 
+# Plot PSFs
 plt.figure()
-plt.xlabel('r')
-plt.ylabel('Point-spread function')
-plt.title('Different point-spread functions, normalised such that their maximum value is 1')
-plt.plot(r, PSF_pl(r, beta, gamma, eta, sigma, nu)[1], label='power law + Gaussian')
-plt.plot(r, PSF_lr(r, beta, eta)[1], label='long range Gaussian only')
+plt.xlabel(r'r [$\mu \mathrm{m}$]')
+plt.ylabel(r'PSF [$\mu \mathrm{m} ^{-2}$]')
+plt.title('Normlised point-spread functions')
+plt.plot(PSF_pl(), label='power law + Gaussian')
+plt.plot(PSF_lr(), label='long range Gaussian only')
 plt.legend()
 
+'''
 # Maps out dose pattern WITHOUT scattering for a number of pointlike features at r=r_f
 # Dose at each r value is either 1 or 0
 r_f = np.array([10, 20, 30, 40, 80, 100, 150, 190])       # the r-position of the features (um)
@@ -69,5 +74,5 @@ plt.title('Dose pattern without electron scattering')
 
 plt.figure()
 plt.plot(np.convolve(D_i, PSF_lr(r, beta=5, eta=eta)[1])) # , label='$\beta$ = {}'.format(b))
-
+'''
 plt.show()
