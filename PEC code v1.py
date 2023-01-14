cb02ae2624b2450408acfdcad4b0c6bfe83b2726
jrseph <ppyjo3@nottingham.ac.uk>
2023-01-14 21:32:38 +0000

    Update PEC code v1.py
    minor changes

--- PEC code v1.py
+++ PEC code v1.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate, signal
-import timeit
 
 
 # -------- Parameters --------
@@ -11,8 +10,8 @@ gran = 100  # Granularity (nm)
 cell_size = gran/1000  # Convert to um
 
 # Scattering parameters
-beta = 6     # um, long range scattering
-eta = 0.5      # dimensionless
+beta = 6        # um, long range scattering
+eta = 0.5       # dimensionless
 gamma = 8.01    # nm
 sigma = 2.60    # dimensionless
 nu = 18.3       # nm
@@ -21,11 +20,10 @@ nu = 18.3       # nm
 
 # -------- PSFs --------
 
-# Coordinates for PSFs - from -2beta to +2beta
+# Coordinates for PSFs -- from -2beta to +2beta
 r = np.arange(-3*round(beta), 3*round(beta)+cell_size, cell_size)
 
-# Normalised PSFs
-# Each array value corresponds to 100nm
+# Normalised PSFs -- each array value corresponds to 100nm
 PSF_lr = 1/(np.pi*(1+eta)) * eta/(beta**2) * np.exp(-np.abs(r)**2/beta**2)
 PSF_lr = PSF_lr / integrate.simps(PSF_lr)
 PSF_pl = 1/(np.pi*(1+eta)) * (nu**(-2)/(1 + (np.abs(r)/gamma)**sigma) \
@@ -42,7 +40,6 @@ subs_width = 40  # substrate width (um)
 x = np.arange(0, subs_width+cell_size, cell_size)
 
 # Desired exposure pattern
-# Dose at each r value is either 1 or 0
 D_0 = np.zeros(np.size(x))
 startend = np.array([[0, 10],   # Start/end points of desired features (um)
                      [11, 20],
@@ -54,20 +51,20 @@ for i in startend:
 
 
 # -------- Plot PSFs --------
-# fig, ax = plt.subplots()
-# ax.set_xlabel(r'r [$\mu \mathrm{m}$]')
-# ax.set_ylabel(r'PSF [$\mu \mathrm{m} ^{-2}$]')
-# ax.set_title('Normlised point-spread functions')
-# ax.plot(PSF_pl, label='power law + Gaussian')
-# ax.plot(PSF_lr, label='long range Gaussian only')
-# ax.legend()
+fig, ax = plt.subplots()
+ax.set_xlabel(r'r [$\mu \mathrm{m}$]')
+ax.set_ylabel(r'PSF [$\mu \mathrm{m} ^{-2}$]')
+ax.set_title('Normlised point-spread functions')
+ax.plot(PSF_pl, label='power law + Gaussian')
+ax.plot(PSF_lr, label='long range Gaussian only')
+ax.legend()
 
 
 
-# # -------- Plot desired exposure dose --------
-# fig, ax = plt.subplots()
-# ax.set_title('Desired dose pattern as in Watson paper')
-# ax.plot(x, D_0)
+# -------- Plot desired exposure dose --------
+fig, ax = plt.subplots()
+ax.set_title('Desired dose pattern as in Watson paper')
+ax.plot(x, D_0)
 
 
 
@@ -77,18 +74,12 @@ fig.suptitle('Corrected exposure dose')
 axs[0].set_title('Long-range Gaussian only')
 axs[1].set_title('Power law + long-range Gaussian')
 
-tic = timeit.default_timer()
-
 for n, fn in enumerate(PSFs):
     D_i = D_0
     for i in range(4):
         cnv = signal.convolve(D_i, fn, mode='same')
-        D_iplus1 = 2*D_0*(1-cnv) # + cnv
+        D_iplus1 = 2*D_0*(1-cnv) + cnv
         D_i = D_iplus1 / np.max(D_iplus1)  # normalise
-        
     axs[n].plot(x, D_i)
-
-toc = timeit.default_timer()
-print('Time to calculate corrected exposure doses: ', toc-tic)
 
 plt.show()
